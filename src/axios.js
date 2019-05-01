f3dca2057fb2bbc7767d7f61dbfd27caa8267e74
Eloy Viteri <eviteri@Eloys-iMac.local>
2019-04-30 23:19:30 -0400

    added new base url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://api.bhumiphotography.com'
+    //baseURL: 'http://api.bhumiphotography.com'
+    baseURL: 'http://api.eviteri.com'
 });
 
 export default instance;
